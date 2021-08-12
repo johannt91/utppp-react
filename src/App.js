3223f66ad07f63cf8cb912b2ca8fb3c8f51b866a
Johann Taylor <j.taylor1343@gmail.com>
2021-08-11 23:20:21 -0400

    Navigation functionality
    Navbar links now render components when clicked

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 import './App.css';
 import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Programs from "./pages/Programs"
+import People from './pages/People';
+import Media from './pages/Media';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/media" component={Media} />
+            <Route path="/people" component={People} />
+            <Route path="/programs" component={Programs} />
+          </Switch>
+        </header>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
@@ -6,15 +7,24 @@ const Navbar = () => {
             <header className="d-flex flex-wrap justify-content-center align-items-center py-3 mb-4 border-bottom">
             <a href="/" className="d-flex flex-column align-items-start mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
                 <span className="fs-5">University of Toronto <br/>Piano Pedagogy Program</span>
-                {/* <span className="fs-5">Piano Pedagogy Program</span> */}
             </a>
 
             <ul className="nav nav-pills">
-                <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a></li>
-                <li className="nav-item"><a href="#" className="nav-link">Programs</a></li>
-                <li className="nav-item"><a href="#" className="nav-link">People</a></li>
-                <li className="nav-item"><a href="#" className="nav-link">Watch &amp; Listen</a></li>
-                <li className="nav-item"><a href="#" className="nav-link">Teacher Portal</a></li>
+                <Link to="/">
+                    <li className="nav-item nav-link active" aria-current="page">Home</li>
+                </Link>
+                <Link to="/programs">
+                    <li className="nav-item nav-link">Programs</li>
+                </Link>
+                <Link to="/people">
+                    <li className="nav-item nav-link">People</li>
+                </Link>
+                <Link to="/media">
+                    <li className="nav-item nav-link">Watch &amp; Listen</li>
+                </Link>
+                <Link to="/teacherportal">
+                    <li className="nav-item nav-link">Teacher Portal</li>
+                </Link> 
             </ul>
             </header>
         </div>
